d46995db31d38f9079bd2744bcc64ecda7b1fea4
omfgtora <spaminol@gmail.com>
2019-02-20 10:26:13 -0800

    Done.

--- app.rb
+++ app.rb
@@ -7,5 +7,10 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  post '/food' do
+    name = params[:name]
+    food = params[:favorite_food]
+    "My name is #{name}, and I love #{food}"
+  end
 
 end
